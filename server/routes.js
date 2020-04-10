f1812e392fe511850650be8434e9e7113c966fb8
Matthew Brown <matthewbrown0798@gmail.com>
2020-04-10 16:31:02 -0400

    added preliminary routes

--- server/routes.js
+++ server/routes.js
@@ -2,5 +2,11 @@ const router = require("express").Router();
 const controller = require("./controllers");
 
 router.get("/", controller.getAllQuestions);
+router.post("/addQuestion", controller.addQuestion);
+router.post("/addAnswer", controller.addAnswer);
+router.post("/reportQuestion", controller.reportQuestion);
+router.post("/reportAnswer", controller.reportAnswer);
+router.post("/markAnswerHelpful", controller.markAnswerHelpful);
+router.post("/markQuestionHelpful", controller.markQuestionHelpful);
 
 module.exports = router;
